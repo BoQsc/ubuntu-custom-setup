db58d7e6be40c3aeac0ec7989f9fe51bd538b5b4
Feldwor <BoQsc@users.noreply.github.com>
2018-07-21 19:02:54 +0000

    Add files via upload

---
+++ setup_ubuntu.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+sudo add-apt-repository universe # only for standard Ubuntu
+sudo apt-get install chrome-gnome-shell
+sudo apt install unity-tweak-tool
---
+++ setup_unity_tweak_tool.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+sudo apt-get install notify-osd
+sudo apt-get install overlay-scrollbar
+
+sudo apt-get install unity-tweak-tool 
