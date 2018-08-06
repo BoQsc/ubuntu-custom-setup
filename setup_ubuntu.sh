1bbf1bbd7aaa9c965fe8cd8e9dda4e52af2c041d
Feldwor <BoQsc@users.noreply.github.com>
2018-08-06 10:16:51 +0300

    Update setup_ubuntu.sh

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo add-apt-repository universe # only for standard Ubuntu44
+sudo add-apt-repository universe # only for standard Ubuntu
 sudo apt-get update
 sudo apt-get install chrome-gnome-shell
 sudo apt install unity-tweak-tool
