9068de689bad810baba2abde57b443f3d22f8556
Feldwor <BoQsc@users.noreply.github.com>
2018-09-13 13:18:48 +0300

    Update extension.js

--- GnomeExtension/example@extensions.gnome.org/extension.js
+++ GnomeExtension/example@extensions.gnome.org/extension.js
@@ -16,7 +16,7 @@
 
 // St means Gnome Shell toolkit.
 // https://developer.gnome.org/st/stable/
-const St = imports.gi.St; // importing from GObject-Introspection namespace
+const St = imports.gi.St; // importing namespace from GObject-Introspection
 const Main = imports.ui.main; // https://gitlab.gnome.org/GNOME/gnome-shell/tree/master/js/ui/main.js
 const Tweener = imports.ui.tweener; // https://gitlab.gnome.org/GNOME/gnome-shell/tree/master/js/ui/tweener.js
 
