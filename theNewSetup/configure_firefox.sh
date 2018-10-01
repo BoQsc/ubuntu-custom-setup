c9dcd87a17af1179b9d8f5fe0d494e9286cffd7c
Feldwor <BoQsc@users.noreply.github.com>
2018-10-01 18:28:53 +0000

    Add files via upload

---
+++ theNewSetup/configure_firefox.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+sudo apt-add-repository multiverse && sudo apt-get update
+sudo apt-get install chrome-gnome-shell
+
+sudo mkdir /usr/lib/firefox/browser/extensions/chrome-gnome-shell@gnome.org
+sudo unzip gnome_shell_integration-10.1-an+fx-linuz.xpi -d /usr/lib/firefox/browser/extensions/chrome-gnome-shell@gnome.org
+
+#/usr/lib/firefox/browser/defaults/preferences
+# change: pref("extensions.autoDisableScopes", 3);
+# to: lockpref("extensions.autoDisableScopes", 0);
+
+
+
+# sudo rm -rf /usr/lib/firefox/browser/extensions/chrome-gnome-shell@gnome.org
+
+
+#  https://addons.mozilla.org/firefox/downloads/file/898030/gnome_shell_integration-10.1-an+fx-linux.xpi?src=dp-btn-primary
