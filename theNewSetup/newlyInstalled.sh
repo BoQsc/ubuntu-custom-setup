7eb9e6fb9f7dbcd789c10f271241d535f70d8118
Feldwor <BoQsc@users.noreply.github.com>
2018-10-03 12:57:17 +0000

    Update newlyInstalled.sh

--- theNewSetup/newlyInstalled.sh
+++ theNewSetup/newlyInstalled.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 sudo apt update && sudo apt upgrade
+
+# Remove The Amazon Files Manually
+    sudo rm /usr/share/applications/ubuntu-amazon-default.desktop
+    sudo rm /usr/share/unity-webapps/userscripts/unity-webapps-amazon/Amazon.user.js
+    sudo rm /usr/share/unity-webapps/userscripts/unity-webapps-amazon/manifest.json
+
