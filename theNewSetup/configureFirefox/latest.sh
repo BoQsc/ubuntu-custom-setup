4588798fdd4b96723911ee81799f8bb8226befa5
Feldwor <BoQsc@users.noreply.github.com>
2018-10-03 17:26:04 +0000

    Update latest.sh

--- theNewSetup/configureFirefox/latest.sh
+++ theNewSetup/configureFirefox/latest.sh
@@ -27,7 +27,6 @@ sudo unzip gnome_shell_integration-an+fx-linuz.xpi -d /usr/lib/firefox/browser/e
 
 
 # Disable Firefox Title-bar
-
 printf '
 defaultPref("browser.tabs.drawInTitlebar", true);
 pref("browser.tabs.drawInTitlebar", true);
