1c7465d6c29cebc08c58bfbb73dde8232f3c022a
Feldwor <BoQsc@users.noreply.github.com>
2018-10-13 08:26:35 +0000

    Update setupGeneralMouseOptions.sh

--- modularized/setupGeneralMouseOptions.sh
+++ modularized/setupGeneralMouseOptions.sh
@@ -1,5 +1,25 @@
 function setupGeneralMouseOptions(){
 
+
+
+## Source of the script: https://stackoverflow.com/questions/18755967/how-to-make-a-program-that-finds-ids-of-xinput-devices-and-sets-xinput-some-set/18756948#18756948
+if [ "$SEARCH" = "" ]; then 
+    exit 1
+fi
+
+ids=$(xinput --list | awk -v search="$SEARCH" \
+    '$0 ~ search {match($0, /id=[0-9]+/);\
+                  if (RSTART) \
+                    print substr($0, RSTART+3, RLENGTH-3)\
+                 }'\
+     )
+
+for i in $ids
+do
+    xinput set-prop $i 'Accel Speed' -0.496403
+done
+##
+
 sudo mkdir /etc/X11/xorg.conf.d
 
 printf '
